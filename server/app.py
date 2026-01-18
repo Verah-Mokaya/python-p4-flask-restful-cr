0f854dcbc40f0754c812619a3692a7672a67356f
Verah Mokaya <verahmokaya475@gmail.com>
2026-01-18 20:51:17 +0300

    done flask-restful api assignment

--- server/app.py
+++ server/app.py
@@ -19,5 +19,38 @@ api = Api(app)
 class Home(Resource):
     pass
 
+class Newsletters(Resource):
+
+    def get(self):
+        response_dict_list = [newsletter.to_dict() for newsletter in Newsletter.query.all()]
+        response = make_response(response_dict_list, 200)
+        return response
+
+    def post(self):
+        data = request.get_json()
+        new_newsletter = Newsletter(
+            title=data.get('title'),
+            description=data.get('description'),
+            frequency=data.get('frequency')
+        )
+        db.session.add(new_newsletter)
+        db.session.commit()
+        response = make_response(new_newsletter.to_dict(), 201)
+        return response
+    
+api.add_resource(Newsletters, '/newsletters')
+
+class NewletterByID(Resource):
+
+    def get(self, id):
+        newsletter = Newsletter.query.get(id)
+        if newsletter:
+            response = make_response(newsletter.to_dict(), 200)
+        else:
+            response = make_response({'error': 'Newsletter not found'}, 404)
+        return response
+
+api.add_resource(NewletterByID, '/newsletters/<int:id>')
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
